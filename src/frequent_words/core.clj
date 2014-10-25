b2c144887d8984c97d711e7fb0532820b744f796
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 18:40:35 +0400

    initial main

--- src/frequent_words/core.clj
+++ src/frequent_words/core.clj
@@ -1,1 +1,18 @@
-(ns frequent_words.core)
+(ns frequent_words.core
+  {:doc "frequent words"}
+  (:use clj-getopts.core)
+  (:use clojure.tools.trace)
+  (:require pattern_count.core)
+  (:gen-class)
+  )
+
+;;(trace-ns 'frequent_words.filter)
+
+(defn -main [& args]
+  (let [opts (getopts (options "is" {:infile :arg
+                                     :k k
+                                     :skip :arg}) args)
+        res (pattern_count.core/read-data opts)
+        ]
+    (println "res:" res)))
+
