3745423cb7f3bc58aaa3eeec65bced44f78f1ca6
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 18:20:10 +0400

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject frequent_words "1.0.0-SNAPSHOT"
-  :description "pattern count. Bioinformatics algorithms"
+  :description "frequent words. Bioinformatics algorithms"
   :main frequent_words.core
   :dependencies [
                   [org.clojure/clojure "1.4.0"]
