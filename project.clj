0e83a2c44650d8a73b255016e239578a344de953
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 18:40:06 +0400

    add extra-classpath-dirs for lein

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject frequent_words "1.0.0-SNAPSHOT"
   :description "frequent words. Bioinformatics algorithms"
   :main frequent_words.core
+  :extra-classpath-dirs [
+                         ".lein-git-deps/pattern_count/src"
+                         ]
   :dependencies [
                   [org.clojure/clojure "1.4.0"]
                   [clj-getopts "0.0.2"]
